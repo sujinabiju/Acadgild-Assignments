32a49c153dcd13a054618ddae4ea83d547e20d96
sujinabiju <30667007+sujinabiju@users.noreply.github.com>
2018-07-15 21:46:07 +0530

    Add files via upload

---
+++ IstAssigmentg.py
@@ -0,0 +1,56 @@
+
+# coding: utf-8
+
+# In[17]:
+
+
+num=[]
+for i in range(2000,3201):
+    if(i%7==0)&(i%5!=0):
+        num.append(str(i))
+print(','.join(num))
+        
+
+
+# In[18]:
+
+
+firstname=input("What is your firstname?")
+lastname=input("What is your lastname?")
+name=firstname + " "+lastname
+name[::-1]
+
+
+# In[19]:
+
+
+d=12
+r=d/2
+volume=4*3.14*r**3/3
+print(volume)
+
+
+# In[20]:
+
+
+
+numbers=input("Enter the numbers of your choice seperated by commas...").split(",")
+numbers
+
+
+# In[21]:
+
+
+symbol='*'
+for i in range(1,6):
+    print(symbol*i)
+for i in reversed(range(1,5)):
+    print(symbol*i)
+
+
+# In[22]:
+
+
+word=input("Enter a word of your choice...")
+word[::-1]
+
